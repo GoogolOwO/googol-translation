4b525c656abd2950caf1f85503a4278f435065e6
Googol <googology+github@outlook.com>
2024-07-02 21:03:03 +0800

    345345

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function translation(str){
     return str.replace("are ", "is ").replace(" are", " is")
               .replace("am ", "is ").replace(" am", " is")
+              .replace("github ", "GitHub ").replace(" github", " GitHub")
 }
