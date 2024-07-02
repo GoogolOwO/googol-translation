124aad7736718a8c612e68b19cc72504a9b5ee3f
Googol <googology+github@outlook.com>
2024-07-02 21:02:38 +0800

    Delete `;`

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function translation(str){
     return str.replace("are ", "is ").replace(" are", " is")
-              .replace("am ", "is ").replace(" am", " is");
+              .replace("am ", "is ").replace(" am", " is")
 }
