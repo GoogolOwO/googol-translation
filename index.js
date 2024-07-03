fa6404bdf44620afa257208459dae75efc72f4b5
Googol <googology+github@outlook.com>
2024-07-03 08:21:25 +0800

    addition word library
    sb22c

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function translation(str){
     return str.replace("are ", "is ").replace(" are", " is")
               .replace("am ", "is ").replace(" am", " is")
               .replace("github ", "GitHub ").replace(" github", " GitHub")
+              .replace("WikiDot ", "Wikidot ").replace(" WikiDot", " Wikidot")
 }
