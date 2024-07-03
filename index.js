e4fd479c52962ab8103bd9fdb1445e2285d1996b
Googol <googology+github@outlook.com>
2024-07-03 08:43:29 +0800

    jsDoC

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Translate from English to Googolish
+ * @param {string} str Original string
+ * @returns Result (To Googolish)
+ */
 function translation(str){
     return str.replace("are ", "is ").replace(" are", " is")
               .replace("am ", "is ").replace(" am", " is")
